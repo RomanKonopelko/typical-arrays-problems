ec6b1756f2089a5c05444653a21510db62a6f81f
RomanKonopelko <romank761@gmail.com>
2021-02-16 22:09:40 +0200

    feat: add tast solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+exports.min = function min(array) {
+    return Array.isArray(array) && array.length > 0 ? Math.min(...array) : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return Array.isArray(array) && array.length > 0 ? Math.max(...array) : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return Array.isArray(array) && array.length > 0
+        ? array.reduce((num, acc) => {
+              return acc + num;
+          }, 0) / array.length
+        : 0;
+};
